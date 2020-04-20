7d759aec701cf5253dfd1f9d9952c85b01a13fbf
Isaac Peiris <isaacpeiris@icloud.com>
2020-04-20 19:02:10 +1000

    Removed graphics, added gradients

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,5 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             nav.classList.add('transparent');
         }
+    });
+
+    const heroSection = document.querySelector('section#hero');
+    heroSection.addEventListener('mousemove', function(event) {
+        let mouseXpercentage = Math.round(event.pageX / heroSection.offsetWidth * 100);
+        let mouseYpercentage = Math.round(event.pageY / heroSection.offsetHeight * 100);
+        heroSection.style.cssText = `background: radial-gradient(at ${mouseXpercentage}% ${mouseYpercentage}%, var(--c-secondary), var(--c-background));`
     })
 })
