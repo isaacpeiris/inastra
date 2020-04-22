11cac8bb56d4fcadcb92870fd12abcd87ea6fa02
Isaac Peiris <isaacpeiris@icloud.com>
2020-04-22 22:43:19 +1000

    - Added webfonts - Redesigned layout

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const nav = document.querySelector('nav');
-    window.addEventListener('scroll', function() {
-        if (this.scrollY > 100) {
-            nav.classList.remove('transparent');
-        } else {
-            nav.classList.add('transparent');
-        }
-    });
+    particlesJS.load('particle-js', 'components/particles-js/particlesjs-config.json');
+})
 
-    particlesJS.load('hero', 'components/particles-js/particlesjs-config.json');
+window.addEventListener('resize', function() {
+    particlesJS.load('particle-js', 'components/particles-js/particlesjs-config.json');
 })
