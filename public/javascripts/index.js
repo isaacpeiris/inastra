052ebe5434cd76aba7721c6179ac7f929dbbe270
Isaac Peiris <isaacpeiris@icloud.com>
2020-04-22 17:01:06 +1000

    Particle JS init

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,10 +8,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    const heroSection = document.querySelector('section#hero');
-    heroSection.addEventListener('mousemove', function(event) {
-        let mouseXpercentage = Math.round(event.pageX / heroSection.offsetWidth * 100);
-        let mouseYpercentage = Math.round(event.pageY / heroSection.offsetHeight * 100);
-        heroSection.style.cssText = `background: radial-gradient(at ${mouseXpercentage}% ${mouseYpercentage}%, var(--c-secondary), var(--c-background));`
-    })
+    particlesJS.load('hero', 'components/particles-js/particlesjs-config.json');
 })
