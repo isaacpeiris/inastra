f852a4d656212f11ddc51787151fb6375dd1f9f1
Isaac Peiris <isaacpeiris@icloud.com>
2020-05-01 14:31:25 +1000

    Changed title structure

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Innova' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 router.get('/confirmed', function(req, res, next) {
