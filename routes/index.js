b26b130ee045ce5934ea0788bb8fc0ef104a41d8
Isaac Peiris <isaacpeiris@icloud.com>
2020-05-01 14:32:01 +1000

    Fixed standard routes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
-});
-
 router.get('/confirmed', function(req, res, next) {
   res.render('confirmed', { title: 'Thank You' });
 });
