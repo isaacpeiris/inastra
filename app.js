c1481b985961f1a574239865741c2cd7d85adea8
Isaac Peiris <isaacpeiris@icloud.com>
2020-05-02 13:55:44 +1000

    Updated error page

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: err.statusCode + ' | ' + err.message });
+  res.render('error', { title: err.statusCode + ' ' + err.message });
 });
 
 module.exports = app;
