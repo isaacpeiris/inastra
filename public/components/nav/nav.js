967edb1b307c1f8ef9f46de6084096b5becffb72
Isaac Peiris <isaacpeiris@icloud.com>
2020-05-04 17:02:17 +1000

    Added mobile nav

--- public/components/nav/nav.js
+++ public/components/nav/nav.js
@@ -9,4 +9,40 @@ document.addEventListener('DOMContentLoaded', function() {
             nav.classList.add('transparent');
         }
     })
+
+    // Mobile navigation variables
+    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileMenuBtn = document.querySelector('button#menu')
+    const mobileMenuIcon = mobileMenuBtn.getElementsByClassName('material-icons')[0];
+    const mobileMenuItems = document.querySelectorAll('.mobile-nav .nav-item');
+
+    // function to open the mobile nav bar
+    function openMobileNav() {
+        mobileNav.classList.add('active');
+        mobileMenuIcon.innerText = 'close';
+        nav.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--c-secondary');
+    }
+
+    // function to close the mobile nav bar
+    function closeMobileNav() {
+        mobileNav.classList.remove('active');
+        mobileMenuIcon.innerText = 'menu';
+        nav.style.backgroundColor = 'transparent';
+    }
+
+    // on click of mobile nav button, if mobile nav is open then close it, otherwise open it
+    mobileMenuBtn.addEventListener('click', function() {
+        if (mobileNav.classList.contains('active')) {
+            closeMobileNav();
+        } else {
+            openMobileNav();
+        }
+    })
+
+    // on click of mobile nav item, close the nav bar
+    mobileMenuItems.forEach(e => {
+        e.addEventListener('click', function() {
+            closeMobileNav();
+        })
+    })
 })
