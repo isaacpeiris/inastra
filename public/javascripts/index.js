11fe3365cb574ecec80ca5a5fb055ddd2d9051e0
Isaac Peiris <isaacpeiris@icloud.com>
2020-06-27 13:08:13 +1000

    Added contact modal for linkedin leads

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const contactForm = document.querySelector('form#contact');
-    contactForm.addEventListener('submit', function() {
-        const formValues = {
-            fullName: contactForm.elements[0].value,
-            firstName: contactForm.elements[0].value.split(' ')[0],
-            email: contactForm.elements[1].value,
-            company: contactForm.elements[2].value,
-            phone: contactForm.elements[3].value
-        }
-        localStorage.setItem('contact', JSON.stringify(formValues));
+    const contactForms = document.querySelectorAll('form.contact');
+    contactForms.forEach(form => {
+        form.addEventListener('submit', function() {
+            console.log('submitted')
+            const formValues = {
+                fullName: form.elements[0].value,
+                firstName: form.elements[0].value.split(' ')[0],
+                email: form.elements[1].value,
+                company: form.elements[2].value,
+                phone: form.elements[3].value
+            }
+            localStorage.setItem('contact', JSON.stringify(formValues));
+        });
     });
+
+    // Focus on form when link is clicked
     document.querySelector('#hero-cta').addEventListener('click', function() {
         setTimeout(function() {
             document.querySelector('form#contact input#name').focus();
