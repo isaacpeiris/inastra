0e362b2f59f218b8458922c9c50788f8ae578f43
Isaac Peiris <isaacpeiris@icloud.com>
2020-08-17 14:40:24 +1000

    Updated accordion styling

--- public/design-system/design-system.js
+++ public/design-system/design-system.js
@@ -140,31 +140,30 @@ document.addEventListener("DOMContentLoaded", function() {
             accordion.querySelectorAll('.accordion-item').forEach(item => {
                 // Wrap item content
                 let itemContent = item.innerHTML;
-                item.innerHTML = `<div class="accordion-item-content">${itemContent}</div>`
+                item.innerHTML = `<div class="accordion-content">${itemContent}</div>`
 
                 // Set shown header
                 item.insertAdjacentHTML('afterbegin',`
-                <div class="accordion-item-header">
-                    <p>${item.dataset.text}</p>
+                <div class="accordion-header">
+                    <p class="accordion-header-text">${item.dataset.text}</p>
                     <span class="material-icons">expand_more</span>
                 </div>
                 `)
 
                 // Toggle active class
-                item.querySelector('.accordion-item-header').addEventListener('click', function() {
+                item.querySelector('.accordion-header').addEventListener('click', function() {
                     if (item.classList.contains('active')) {
                         item.classList.remove('active');
-                        item.querySelector('.accordion-item-header .material-icons').innerText = 'expand_more';
+                        item.querySelector('.accordion-header .material-icons').innerText = 'expand_more';
                     } else {
                         if (!accordion.classList.contains('expandable')) {
                             accordion.querySelectorAll('.accordion-item').forEach(item => {
                                 item.classList.remove('active');
-                                item.querySelector('.accordion-item-header .material-icons').innerText = 'expand_more';
-
+                                item.querySelector('.accordion-header .material-icons').innerText = 'expand_more';
                             })
                         }
                         item.classList.add('active');
-                        item.querySelector('.accordion-item-header .material-icons').innerText = 'expand_less';
+                        item.querySelector('.accordion-header .material-icons').innerText = 'expand_less';
                     }
                 })
             });
