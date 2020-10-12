11546bb5cc33dfdef8339205a811578e23dece52
Isaac Peiris <isaacpeiris@icloud.com>
2020-10-12 15:38:42 +1100

    New landing page

--- public/javascripts/marketing.js
+++ public/javascripts/marketing.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     // C19
-    let params = getParams();
-    if (params) {
-        if (params.utm_campaign == 'c19') {
-            setTimeout(function() {
-                openModal('linkedin-contact')
-            }, 6000)
-        }
-    }
+    // let params = getParams();
+    // if (params) {
+    //     if (params.utm_campaign == 'c19') {
+    //         setTimeout(function() {
+    //             openModal('linkedin-contact')
+    //         }, 6000)
+    //     }
+    // }
 });
 
 function getParams() {
     // Get params from current url
-    const paramString = window.location.search.replace('?','')
+    const paramString = window.location.search.replace('?', '')
     if (paramString != '') {
         let paramObj = {};
         paramString.split('&').forEach(param => {
