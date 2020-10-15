e507c4119d45c21a428e732c63422c5958a830cb
Isaac Peiris <isaacpeiris@icloud.com>
2020-10-15 18:37:42 +1100

    Serve content dynamically from json

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const caseStudies = require('../content/case-studies.json');
+
+function encodeTitle(title) {
+    let lowerCase = title.toLowerCase();
+    let noSpace = lowerCase.replace(/\s/g, '-');
+    return noSpace;
+}
+
+function getCaseStudy(id) {
+
+}
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Transdisciplinary Innovation' });
@@ -13,8 +24,24 @@ router.get('/legal/privacy-policy', function(req, res, next) {
     res.render('legal/privacy-policy', { title: 'Privacy Policy' });
 });
 
-router.get('/case-studies/ihg', function(req, res, next) {
-    res.render('case-studies/ihg', { title: 'IHG' });
+router.get('/case-studies/:id', function(req, res, next) {
+    const doc = caseStudies.find(obj => {
+        return obj.id === req.params.id
+    });
+    if (doc) {
+        res.render('case-studies', {
+            title: doc.organisation,
+            caseStudy: doc
+        });
+    } else {
+        res.render('error', {
+            title: '404 Not Found',
+            error: {
+                status: 404
+            },
+            message: "Not Found"
+        });
+    }
 });
 
 module.exports = router;
