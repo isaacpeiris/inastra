5e1919360593950ed398fc39d3036af9a641a846
Isaac Peiris <isaacpeiris@icloud.com>
2020-11-21 12:58:25 +1100

    Longer duration for values timer

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const descriptionArray = Array.prototype.slice.call(valuesDescriptions);
     const values = document.querySelector('.values-wrapper').children;
     const valuesArray = Array.prototype.slice.call(values);
-    const timerDuration = 8000;
+    const timerDuration = 12000;
 
     let valuesTimer = setInterval(valuesCounter, timerDuration);
 
@@ -40,4 +40,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
         }
     }
-})
+});
